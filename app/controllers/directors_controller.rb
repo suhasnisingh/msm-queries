cde3d9b5bd42abc2c473f4ef75250b2a394dd335
suhasnisingh <165236685+suhasnisingh@users.noreply.github.com>
2024-04-28 07:22:59 +0000

    completed details pages

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -6,7 +6,7 @@ class DirectorsController < ApplicationController
   def show
     the_id = params.fetch("the_id")
     matching_records = Director.where({ :id => the_id })
-    @x = matching_records.at(0)
+    @the_director = matching_records.at(0)
     render({ :template => "director_templates/details"})
   end
 end
