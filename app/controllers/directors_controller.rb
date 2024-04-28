deaf3b248af008cf62892315f93354bf71c9ff55
suhasnisingh <165236685+suhasnisingh@users.noreply.github.com>
2024-04-28 07:48:41 +0000

    Added youngest and eldest

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -9,4 +9,17 @@ class DirectorsController < ApplicationController
     @the_director = matching_records.at(0)
     render({ :template => "director_templates/details"})
   end
+
+  def youngest
+     youngest_records = Director.where.not({ :dob => nil }).order({ :dob => :desc })
+     @youngest_director = youngest_records.at(0)
+    render({ :template => "director_templates/youngest"})
+  end
+
+  def eldest
+    eldest_records = Director.where.not({ :dob => nil }).order({ :dob => :asc })
+    @eldest_director = eldest_records.at(0)
+    render({ :template => "director_templates/eldest"})
+  end
+
 end
